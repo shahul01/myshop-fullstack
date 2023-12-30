64ddb3c221c689b9bdcac6567ff8332be626a9e0
Shahul Hammeed <53307156+shahul01@users.noreply.github.com>
2023-12-30 19:34:27 +0530

    feat:fe: added main 3 products in home

--- frontend/src/app/(routes)/page.tsx
+++ frontend/src/app/(routes)/page.tsx
@@ -1,8 +1,37 @@
+import Image from "next/image";
 
 export default function Home() {
+
+
+
   return (
     <main className="text-blue-400">
-      Hello world
+      <div className="home-hero">
+        <div>
+          <Image
+            src={'/images/products/coat.png'}
+            alt='Coat'
+            height={100}
+            width={100}
+          />
+        </div>
+        <div>
+          <Image
+            src={'/images/products/turtleneck.png'}
+            alt='Coat'
+            height={100}
+            width={100}
+          />
+        </div>
+        <div>
+          <Image
+            src={'/images/products/pant.png'}
+            alt='Coat'
+            height={100}
+            width={100}
+          />
+        </div>
+      </div>
     </main>
   )
-}
+};
